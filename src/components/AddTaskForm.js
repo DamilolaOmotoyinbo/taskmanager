2619d29f3c35fe680dca5009a051f0fbcaa8700c
DamilolaOmotoyinbo <damilolaomotoyinbo@gmail.com>
2024-02-29 01:00:54 +0100

    work

--- src/components/AddTaskForm.js
+++ src/components/AddTaskForm.js
@@ -1,10 +1,16 @@
 import React, {useState} from 'react'
+import { TaskList } from './TaskList';
 
-export const AddTaskForm = () => {
+export const AddTaskForm = ({addTask}) => {
     const [value, setValue] = useState("")
+
+    const handleSubmit = e => {
+        e.preventDefault();
+        addTask(value)
+    }
   return (
-    <form className='TaskForm'>
-        <input type='text' className='taskinput' placeholder='Input your task'onChange={handleChange}/>
+    <form className='TaskForm' onSubmit={handleSubmit}>
+        <input type='text' className='taskinput' placeholder='Input your task here'onChange={(e) => setValue (e.target.value)}/>
         <button type='submit' className='task-btn'>Add Task</button>
     </form>
   )
--- src/components/TaskList.js
+++ src/components/TaskList.js
@@ -1,10 +1,17 @@
-import React from 'react'
+import React, {useState} from 'react'
 import { AddTaskForm } from './AddTaskForm'
+import {v4 as uuidv4 } from 'uuid';
+uuidv4();
 
 export const TaskList = () => {
+    const {tasks, setTasks}= useState([])
+    const addTask = task => {
+    setTasks([...tasks, { id: uuidv4(), task: tasks, completed: false, isEditing: false }]);
+    console.log(tasks);
+  }
   return (
     <div className='TaskList'>
-        <AddTaskForm/>
+        <AddTaskForm addTask={addTask}/>
     </div>
   )
 }
