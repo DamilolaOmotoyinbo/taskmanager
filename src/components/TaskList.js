4df964b2f6deab80762a183c5b83e0de551304b3
DamilolaOmotoyinbo <damilolaomotoyinbo@gmail.com>
2024-02-29 01:05:04 +0100

    work

--- src/components/TaskList.js
+++ src/components/TaskList.js
@@ -1,18 +1,20 @@
-import React, {useState} from 'react'
-import { AddTaskForm } from './AddTaskForm'
-import {v4 as uuidv4 } from 'uuid';
+import React, { useState } from "react";
+import { AddTaskForm } from "./AddTaskForm";
+import { v4 as uuidv4 } from "uuid";
 uuidv4();
 
 export const TaskList = () => {
-    const {tasks, setTasks}= useState([])
-    const addTask = task => {
-    setTasks([...tasks, { id: uuidv4(), task: tasks, completed: false, isEditing: false }]);
+  const { tasks, setTasks } = useState([]);
+  const addTask = (task) => {
+    setTasks([
+      ...tasks,
+      { id: uuidv4(), task: tasks, completed: false, isEditing: false },
+    ]);
     console.log(tasks);
-  }
+  };
   return (
-    <div className='TaskList'>
-        <AddTaskForm addTask={addTask}/>
+    <div className="TaskList">
+      <AddTaskForm addTask={addTask} />
     </div>
-  )
-}
-
+  );
+};
